5a28073cbacf33d7ae920963262b6c9e11c559f2
KseniyaNemtyreva <k.nemtyryova@yandex.ru>
2023-03-02 10:24:27 +0300

    upgrade_dvoroz

--- start_parce/dvoroz.py
+++ start_parce/dvoroz.py
@@ -9,53 +9,20 @@ import re
 
 
 carts = []
-
-list_cat_url = []
 count = 0
 
 url = "http://dvoroz.ru/sitemap_tovar.xml"
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.3.949 Yowser/2.5 Safari/537.36"
+    "accept": "*/*",
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.2.934 Yowser/2.5 Safari/537.36",
 }
+
 req = requests.get(url, headers=headers)
+
 soup = BeautifulSoup(req.text, "xml")
 
-list_catigories_url = soup.find_all('loc')
-for item in list_catigories_url:
-    item = item.getText()
-    list_cat_url.append(item)
 
-
-# Цикл для перехода по карточкам
-for item in tqdm(list_cat_url):
-    req = requests.get(item, headers=headers)
-    soup = BeautifulSoup(req.text, "lxml")
-
-    count += 1
-    print(f"Обработка {count} карточки")
-
-    try:
-        url = item
-    except Exception:
-        url = 'none'
-    try:
-        name = soup.find('h1', class_="text-center").getText()
-    except Exception:
-        name = 'none'
-    try:
-        print()
-        # price = re.search('[0-9.]+', soup.find('div', class_="treeview").getText())
-    except Exception:
-        price = 'none'
-
-    carts.append({
-        "url": url,
-        "name": name,
-        # "price": price.group(0),
-    })
-    print(carts)
-    exit()
-# print(carts)
+print(carts)
 
 
     # with open("../resul_parce/carts_discont.json", "w") as file:
